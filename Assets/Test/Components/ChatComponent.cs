1b4d349ac0b34b0479fbb985d7ded102fdd443bd
Icarus <1375400884@qq.com>
2019-06-14 01:37:48 +0800

    !T 1.0

--- Assets/Test/Components/ChatComponent.cs
+++ Assets/Test/Components/ChatComponent.cs
@@ -5,6 +5,7 @@
 //Assembly-CSharp
 
 using System;
+using System.Threading.Tasks;
 using Grpc.Core;
 using MagicOnion.Client;
 using MagicOnionTestService.LobbyMessageTest;
@@ -15,53 +16,115 @@ namespace Test.Components
 {
     public class ChatComponent : MonoBehaviour, IChat
     {
+        string _playerName;
         private IChatHub _chatHub;
         private bool _isJoin;
 
         public InputField UserNameInputField;
+        public InputField RoomName;
         
         //显示消息
         public Text ChatText;
 
         //入室・退室UI
-        public Button JoinOrLeaveButton;
         public Text JoinOrLeaveButtonText;
 
         //发送消息块
         public Button SendMessageButton;
         public InputField MessageInputField;
 
+        [Header("服务器连接相关")] 
+        public InputField ip;
+
+        public InputField port;
+
+        [Header("ui范围")]
+        public GameObject chat;
+        public GameObject server;
+        private Channel _channel;
+
+
         // Start is called before the first frame update
         void Start()
         {
+            chat.SetActive(false);
+            server.SetActive(true);
+            SendMessageButton.enabled = false;
+            
             this._isJoin = false;
+        }
 
-            //初始化
-            var channel = new Channel("localhost:12345", ChannelCredentials.Insecure);
-            this._chatHub = StreamingHubClient.Connect<IChatHub, IChat>(channel, this);
-
-            //默认隐藏发送消息按钮
-            this.SendMessageButton.gameObject.SetActive(false);
+        public void Connect()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(ip.text))
+                {
+                    this.ChatText.text += $"<color=\"red\">\nIP 不能为空!</color>"; 
+                    return;
+                }
+                
+                if (string.IsNullOrWhiteSpace(port.text))
+                {
+                    this.ChatText.text += $"<color=\"red\">\n端口 不能为空!</color>"; 
+                    return;
+                }
+                
+                _channel = new Channel($"{ip.text}:{port.text}", ChannelCredentials.Insecure);
+                
+                this._chatHub = StreamingHubClient.Connect<IChatHub, IChat>(_channel, this);
+                
+                chat.SetActive(true);
+                server.SetActive(false);
+            }
+#pragma warning disable 168
+            catch (Exception e)
+#pragma warning restore 168
+            {
+                this.ChatText.text += $"<color=\"red\">\n服务器连接失败!</color>";
+            }
         }
 
         // Update is called once per frame
-        void Update()
+        async void Update()
         {
         }
 
-        private void OnDestroy()
+        private async void OnDestroy()
         {
-            //被销毁,调用离开
-            this._chatHub.LeaveAsync();
+            await _leave();
+        }
+
+        private async Task _leave()
+        {
+            if (_chatHub == null)
+            {
+                return;
+            }
+            
+            await this._chatHub.LeaveAsync();
+            
+            await _des();
+        }
+
+        private async Task _des()
+        {
+            if (_chatHub == null)
+            {
+                return;
+            }
+            
+            await _chatHub.DisposeAsync();
+            await _channel.ShutdownAsync();
             this._chatHub = null;
+            _channel = null;
         }
 
-        private void OnApplicationQuit()
+        private async void OnApplicationQuit()
         {
             if (_chatHub != null)
             {
-                //游戏结束,调用离开
-                this._chatHub.LeaveAsync();
+                await _leave();
             }
         }
 
@@ -77,21 +140,55 @@ namespace Test.Components
                 await this._chatHub.LeaveAsync();
                 this._isJoin = false;
                 this.JoinOrLeaveButtonText.text = "加入房间";
-                this.SendMessageButton.gameObject.SetActive(false);
-                UserNameInputField.gameObject.SetActive(true);
+                this.SendMessageButton.enabled = false;
+                UserNameInputField.enabled = true;
             }
             else
             {
                 if (string.IsNullOrEmpty(UserNameInputField.text))
                 {
-                    this.ChatText.text += $"<color=\"red\">用户名不能为空..</color>";
+                    this.ChatText.text += $"<color=\"red\">\n用户名不能为空..</color>";
                     return;
                 }
-                await this._chatHub.JoinAsync(this.UserNameInputField.text);
-                this._isJoin = true;
-                this.JoinOrLeaveButtonText.text = "离开房间";
-                this.SendMessageButton.gameObject.SetActive(true);
-                UserNameInputField.gameObject.SetActive(false);
+                
+                if (string.IsNullOrEmpty(RoomName.text))
+                {
+                    this.ChatText.text += $"<color=\"red\">\n房间名名不能为空..</color>";
+                    return;
+                }
+
+                _playerName = UserNameInputField.text;
+
+                try
+                {
+                    await this._chatHub.JoinAsync(this.UserNameInputField.text, RoomName.text);
+                    this._isJoin = true;
+                    this.JoinOrLeaveButtonText.text = "离开房间";
+                    this.SendMessageButton.enabled = true;
+                    UserNameInputField.enabled = false;
+                    RoomName.enabled = false;
+                }
+                catch (RpcException re)
+                {
+                    this.ChatText.text += $"<color=\"red\">\n连接服务器失败..</color>";
+                    chat.SetActive(false);
+
+                    await _des();
+                    
+                    server.SetActive(true);
+                    
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(_chatHub);
+
+                    
+                    Debug.LogException(e);
+                    this.ChatText.text += $"<color=\"red\">\n" +
+                                          $"加入{RoomName.text}房间失败..\n" +
+                                          $"{e.Message}" +
+                                          $"</color>";
+                }
             }
         }
 
@@ -113,6 +210,11 @@ namespace Test.Components
 
         public void OnJoin(string name)
         {
+            if (name == _playerName)
+            {
+                return;
+            }
+            
             this.ChatText.text += $"\n{name}加入房间 - 时间:{DateTime.Now:s}";
         }
 
